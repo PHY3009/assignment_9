d9cb5f5fbb457b2a9ee0e2adbaa928c8cd976356
isaac-menninga <igmenninga@gmail.com>
2015-11-10 20:08:40 -0800

    Removed problem specific function to generalize the script more.

--- plot.py
+++ plot.py
@@ -1,6 +1,5 @@
 # Isaac Menninga, 2015-11-06
-# Read in inflammation data and print the mean inflammation for each 
-patient
+# Read in inflammation data and print the mean inflammation for each patient
 
 #import libraries
 import pandas as pd, sys, matplotlib.pyplot as plt
@@ -13,31 +12,9 @@ def main():
 
 	#load the data
 	data = pd.read_csv(filename, delimiter = ',')
-
-	#call process to get values from action and data
-	values = process(action, data)
 	
-	#print data
-	plot(values, save_path)
-
-def process(command, dataset):	
-	#choose action depending on user input
-	#if user input is '--min', find the min value for each patient
-	if command == '--min':
-		out = dataset.min(axis = 0)
-		
-	#if user input is '--max', find the max value for each patient
-	elif command == '--max':
-		out = dataset.max(axis = 0)
-		
-	#if user input is '--mean', find the mean value for each patient
-	elif command == '--mean':
-		out = dataset.mean(axis = 0)
-	#else, output an error message
-	else:
-		print('Please enter a valid input for action. Valid 
-inputs are: --min, --max, --mean')
-	return out
+	#plot data
+	plot(data, save_path)
 
 def plot(plot_data, savename):
 	#plot data
