fc3ea5b2de76abf47dca1c9e3d0072fdd2f58b1a
msbaumann <miriam.b1@hotmail.com>
2015-11-11 09:24:18 -0800

    Defined a new plot function that plots and saves a figure

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 # Miriam Baumann, 2015-11-10
 # Reads in a dataframe in the form of a simple text file and 
-# plots the mean of each column in the y- axis, and then saves the plot  
+# plots the mean of each column and then saves the plot  
 
 # import libraries
 import pandas as pd
@@ -15,5 +15,17 @@ def main():
 	
 	#load the data using panadas
 	data = pd.read_csv(filename, delimiter=',')
+
 	
+def plot(data):
+
+	#Plotting the mean of each column of the data 
+	plt.plot(data.mean(axis=1))
 	
+	#Saving the plot
+	plt.savefig(savename)
+	
+	plt.show()
+
+	
+main()
