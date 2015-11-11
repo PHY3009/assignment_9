ae295a1f5003922bc284cacaab3d17c2a36bb8a2
msbaumann <miriam.b1@hotmail.com>
2015-11-11 10:19:46 -0800

    Changed what will be plotted, from all the data to just the mean of each column

--- script.py
+++ script.py
@@ -15,7 +15,7 @@ savename = sys.argv[2]
 data = pd.read_csv(filename, delimiter=',')	
 	
 #Plotting the mean of each column of the data 
-plt.plot(data)
+plot = plt.plot(data. mean(axis=1))
 	
 #Saving the plot
 plt.savefig(savename)
