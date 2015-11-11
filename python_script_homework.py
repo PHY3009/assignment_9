e7efe6fd145caa9857578b83516e98c41d688d0c
Jhanelle <jhanelle.williams@questu.ca>
2015-11-11 11:43:30 -0800

    removed unnecessary for the code

--- python_script_homework.py
+++ python_script_homework.py
@@ -1,5 +1,4 @@
-#Jhanelle Williams
-#10-11-2015
+#Jhanelle Williams 10-11-2015
 #Assignment 9
 # The aim of this assignment is to create a python script which loads datasets, plot and save the figures
 
@@ -7,31 +6,22 @@
 
 import sys
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
-def main ():
+def main():
 
 	#assign variables
 	input_file= sys.argv[1]
-	output_file=sys.argv[3]
-	action=sys.argv[2]
+	output_file=sys.argv[2]
+	#action=sys.argv[2]
 
 	#load data
 	sample_data=pd.read_csv(input_file, delimiter=',')
 
-	 #calculation for data
-	values=process(action, sample_data)
-
-	#output data
-	print(values)
-
-	#output data as plot
-	my_plot(action, data)
-
-def plot (plot_data,savename):
 	#plot data
-	plt.plot(plot_data)
+	plt.plot(sample_data)
 	#show plot
-	plt.savefig(savename)
+	plt.savefig(output_file)
 	plt.show()
+
 main()
