1d3f40d4fe8c204868321c0d0603c0bcb0f74f74
anders1216 <anders1216@yahoo.com>
2015-11-11 10:43:05 -0800

    Turning in my homework for today.

---
+++ plat.py
@@ -0,0 +1,45 @@
+#defining the main function which defines the variables, loads the data, the variable functions that process the data.
+
+def main ():
+        
+        #assign variables
+        action = sys.argv[1]
+        filename = sys.argv[2]
+        save_path = sys.argv[3]
+        
+        #load the data
+        data = pd.read_csv(filename, delimiter=',')
+        
+        # call process to get values from action and data
+        values = process(action, data)
+        plot = data_plot(action, data)
+        
+        #output data
+        print (values)
+        print (data_plot)
+        
+        #save figure
+        plt.savefig(save_path)
+
+def process (command, dataframe):
+        #create a command where you can choose the processing method you want depending on user input
+        if command == '--min':
+                out = dataframe.min(axis=1)
+        elif command == '--max':
+                out = dataframe.max(axis=1)
+        elif command == '--mean':
+                out = dataframe.mean(axis=1)
+        else:
+                "Please specify if you would like the min, mean or max values for the data"
+        
+def data_plot (command, dataframe):
+       
+        #plot the data
+        if command == '--min':
+                plot = plt.plot(dataframe.min(axis=1))
+        elif command == '--max':
+                plot = plt.plot(dataframe.max(axis=1))
+        elif command == '--mean':
+                plot = plt.plot(dataframe.mean(axis=1))
+        
+main()
