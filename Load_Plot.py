da01e1b284e117ec0e10c9793b45a95b6f962d8e
jonahgolden <jwgolden@questu.ca>
2015-11-11 11:42:36 -0800

    Adding the Python script, License, and Readme for Assignment 9.

---
+++ Load_Plot.py
@@ -0,0 +1,31 @@
+
+# Jonah Golden, 2015-11-11
+# Script that reads csv data, then shows and saves a plot of that data
+
+# Import libraries
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def main():
+
+	# Assign variables
+	input_file = sys.argv[1]
+	output_file = sys.argv[2]
+
+	# Load the data
+	data = pd.read_csv(input_file, delimiter=',')
+	
+	# Apply the plot function to the loaded data
+	plt.plot(data)
+	
+	# Save the plot
+	plt.savefig(output_file)
+	
+	# Show the plot
+	plt.show()
+
+main()
+
+
+		
