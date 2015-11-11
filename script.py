33945bc89066ab21c3b4f75b42c046689fe689be
msbaumann <miriam.b1@hotmail.com>
2015-11-11 09:15:57 -0800

    Starting over because it wasnt working,imported libraries

--- script.py
+++ script.py
@@ -7,29 +7,3 @@ import pandas as pd
 import sys
 import matplotlib as plt
 
-def main ():
-# assign variables
-	filename = sys.argv[1]
-	savename = sys.argv[2]
-
-	# load the data
-	data = pd.read_csv(filename, delimiter=',')
-
-	# call process to get values from data
-	values = process(command, data)
-	
-	#Saving the figure 
-	plt.savefig(savename)
-	
-def process(command, data):
-	#Defining what to do with the dataframe
-	command = data.mean(axis = 1)
-	
-	
-def plot(data):	
-	
-	plot = plt.plot(dataframe.mean(axis=1))
-	return plot 
-	
-
-main()
