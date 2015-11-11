094265088246fbe008331964a4f53e9c1c0fae81
msbaumann <miriam.b1@hotmail.com>
2015-11-11 00:06:23 -0800

    Created a script that loads a dataset and plots and saves a figure

---
+++ script.py
@@ -0,0 +1,40 @@
+# Miriam Baumann, 2015-11-10
+# Reads in a dataframe in the form of a simple text file and 
+# plots the mean of each column in the y- axis, and then saves the plot  
+
+# import libraries
+import pandas as pd
+import sys
+
+def main ():
+# assign variables
+	filename = sys.argv[1]
+	savename = sys.argv[2]
+
+	# load the data
+	data = pd.read_csv(filename, delimiter=',')
+
+	# call process to get values from data
+	values = process(data)
+
+	#output data
+	print(values)
+	
+	#Saving the figure 
+	plt.savefig(savename)
+	
+def process(data):
+	#Defining what to do with the dataframe
+	out = data.mean(axis = 1)
+	return out
+		
+#def plot(plot_data, savename):
+	#plot data
+	#plt.plot(plot_data)
+	#save plot 
+	#plt.savefig(savename)
+	#show plot
+	#plt.show()
+	
+
+main()
